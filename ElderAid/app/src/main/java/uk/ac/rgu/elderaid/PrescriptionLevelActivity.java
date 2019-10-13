ea2e68a61af45b6be20a488fb6fa3858a1c339a7
Nathan <nathan.lib19@gmail.com>
2019-10-13 11:17:52 +0100

    Fix contact scrollview issues

---
+++ ElderAid/app/src/main/java/uk/ac/rgu/elderaid/PrescriptionLevelActivity.java
@@ -0,0 +1,14 @@
+package uk.ac.rgu.elderaid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PrescriptionLevelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prescription_level);
+    }
+}
