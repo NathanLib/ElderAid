a3dee28bf1145f6e2e594daa16a2be806b5692e6
ItsKelo <clorixian@gmail.com>
2019-10-16 14:23:19 +0100

    Made the maps dialog

--- ElderAid/app/src/main/java/uk/ac/rgu/elderaid/MapsActivity.java
+++ ElderAid/app/src/main/java/uk/ac/rgu/elderaid/MapsActivity.java
@@ -2,13 +2,49 @@ package uk.ac.rgu.elderaid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
-public class MapsActivity extends AppCompatActivity {
+public class MapsActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button btnEditFavourites;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        btnEditFavourites = findViewById(R.id.btnEditFav);
+        btnEditFavourites.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialog();
+            }
+        });
+
     }
+
+    public void openDialog(){
+        final Dialog favDialog = new Dialog(this);
+        favDialog.setContentView(R.layout.dialog_add_favourites);
+        favDialog.setTitle("Edit Favourites");
+
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+        lp.copyFrom(favDialog.getWindow().getAttributes());
+        lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        favDialog.show();
+        favDialog.getWindow().setAttributes(lp);
+
+        favDialog.show();
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
 }
