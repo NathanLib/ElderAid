8d910002ecf5fc5d1d639a476c2ca3a1de2accbf
User <1703589@rgu.ac.uk>
2019-10-17 21:43:35 +0100

    Task checklist activity done

--- ElderAid/app/src/main/java/uk/ac/rgu/elderaid/HomeActivity.java
+++ ElderAid/app/src/main/java/uk/ac/rgu/elderaid/HomeActivity.java
@@ -2,13 +2,48 @@ package uk.ac.rgu.elderaid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageButton;
 
-public class HomeActivity extends AppCompatActivity {
-
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener{
+    private ImageButton btnshowSideNav;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        btnshowSideNav = (ImageButton) findViewById(R.id.btnMenu);
+        btnshowSideNav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openNavDialog();
+            }
+        });
+    }
+
+    public void openNavDialog() {
+        final Dialog sideNavDialog = new Dialog(this);
+
+
+        sideNavDialog.setContentView(R.layout.dialog_side_navigation);
+        sideNavDialog.setTitle("Navigation View");
+
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+        lp.copyFrom(sideNavDialog.getWindow().getAttributes());
+        lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+        lp.height = WindowManager.LayoutParams.MATCH_PARENT;
+
+        sideNavDialog.show();
+        sideNavDialog.getWindow().setAttributes(lp);
+
+        sideNavDialog.show();
+
+    }
+    @Override
+    public void onClick(View v) {
+
     }
 }
