1f57e0a7d52d28f2e03ab40a19c9260ccd13db7f
ItsKelo <clorixian@gmail.com>
2019-12-04 03:26:54 +0000

    Made Datepickers for the manual additions

--- ElderAid/app/src/main/java/uk/ac/rgu/elderaid/CalendarActivity.java
+++ ElderAid/app/src/main/java/uk/ac/rgu/elderaid/CalendarActivity.java
@@ -2,7 +2,9 @@ package uk.ac.rgu.elderaid;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
 import android.app.Dialog;
 
 import android.content.Intent;
@@ -14,6 +16,8 @@ import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -31,11 +35,16 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.lang.reflect.Array;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 
-public class CalendarActivity extends AppCompatActivity implements View.OnClickListener {
+public class CalendarActivity extends AppCompatActivity implements View.OnClickListener , DatePickerDialog.OnDateSetListener {
     private ImageButton btnAddEvent;
     private ImageButton btnshowSideNav;
     private LinearLayout calendar_llEvent1;
@@ -45,6 +54,14 @@ public class CalendarActivity extends AppCompatActivity implements View.OnClickL
     private static final String TAG = "HELLO";
     private static final String preferencesFile = "uk.ac.rgu.elderaid";
     private SharedPreferences sharedPrefs;
+    private EditText etTitle;
+    private EditText etLocation;
+    private EditText etDescription;
+
+    private TextView tvStartDate;
+    private TextView tvEndDate;
+
+    private int dateFlag;
 
 
     // URL template to download data
@@ -58,9 +75,38 @@ public class CalendarActivity extends AppCompatActivity implements View.OnClickL
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
 
+        // *Shared Preferences File link*
         sharedPrefs = getSharedPreferences(preferencesFile, MODE_PRIVATE);
 
+        // *Button initializations*
         btnSOS = (ImageButton) findViewById(R.id.btnSOS);
+        btnHome = (ImageButton) findViewById(R.id.btnHome);
+        btnAddEvent = (ImageButton) findViewById(R.id.calendar_btnAddEvent);
+        btnshowSideNav = (ImageButton) findViewById(R.id.btnMenu);
+        btnSyncEvents = (ImageButton) findViewById(R.id.calendar_btnSyncEvents);
+
+        calendar_llEvent1 = (LinearLayout) findViewById(R.id.calendar_llEvent1);
+
+        // *Database setup*
+        //Get the database instance
+        ElderaidDatabase db = ElderaidDatabase.getDatabase(this);
+        //Get the DAO from the database
+        this.eventDao = db.eDao();
+
+        // *Toolbar setup*
+        // The code below was adapted from a source on the internet from this point.
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("My calendar");
+        setSupportActionBar(toolbar);
+        // To this point.
+
+
+
+
+
+
+
+        // *On click listeners for image buttons*
         btnSOS.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -68,17 +114,15 @@ public class CalendarActivity extends AppCompatActivity implements View.OnClickL
             }
         });
 
-        btnHome = (ImageButton) findViewById(R.id.btnHome);
         btnHome.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view){
                 Intent intent = new Intent(
                         getApplicationContext(), HomeActivity.class);
                 startActivity(intent);
             }
-        });;
+        });
 
-        btnAddEvent = (ImageButton) findViewById(R.id.calendar_btnAddEvent);
         btnAddEvent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -86,7 +130,6 @@ public class CalendarActivity extends AppCompatActivity implements View.OnClickL
             }
         });
 
-        btnshowSideNav = (ImageButton) findViewById(R.id.btnMenu);
         btnshowSideNav.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -94,7 +137,6 @@ public class CalendarActivity extends AppCompatActivity implements View.OnClickL
             }
         });
 
-        calendar_llEvent1 = (LinearLayout) findViewById(R.id.calendar_llEvent1);
         calendar_llEvent1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -102,8 +144,6 @@ public class CalendarActivity extends AppCompatActivity implements View.OnClickL
             }
         });
 
-
-        btnSyncEvents = (ImageButton) findViewById(R.id.calendar_btnSyncEvents);
         btnSyncEvents.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -120,21 +160,6 @@ public class CalendarActivity extends AppCompatActivity implements View.OnClickL
             }
         });
 
-
-        //Get the database instance
-        ElderaidDatabase db = ElderaidDatabase.getDatabase(this);
-        //Get the DAO from the database
-        this.eventDao = db.eDao();
-
-
-
-
-        // The code below was adapted from a source on the internet from this point.
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle("My calendar");
-        setSupportActionBar(toolbar);
-        // To this point.
     }
 
     public void downloadEvents(String CalID){
@@ -270,8 +295,30 @@ public class CalendarActivity extends AppCompatActivity implements View.OnClickL
         lp.copyFrom(dialog.getWindow().getAttributes());
         lp.width = WindowManager.LayoutParams.MATCH_PARENT;
         lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
-        dialog.show();
         dialog.getWindow().setAttributes(lp);
+
+        Button cancelAddButton = (Button) dialog.findViewById(R.id.addevent_btnCancelEvent);
+        cancelAddButton.setOnClickListener(this);
+
+        Button submitAddButton = (Button) dialog.findViewById(R.id.addevent_btnAddEvent);
+        submitAddButton.setOnClickListener(this);
+
+        Button btnAddStartDate = (Button) dialog.findViewById(R.id.btnStartDatePicker);
+        btnAddStartDate.setOnClickListener(this);
+        Button btnAddEndDate = (Button) dialog.findViewById(R.id.btnEndDatePicker);
+        btnAddEndDate.setOnClickListener(this);
+
+
+
+        //Finding the editTexts
+        etTitle = dialog.findViewById(R.id.addevent_etEventName);
+
+
+        etLocation = dialog.findViewById(R.id.addevent_etEventPlace);
+        etDescription = dialog.findViewById(R.id.addevent_etEventDescription);
+        //Finding the viewTexts
+        tvStartDate = dialog.findViewById(R.id.addevent_etEventStartDate);
+        tvEndDate = dialog.findViewById(R.id.addevent_etEventEndDate);
 
         dialog.show();
     }
@@ -307,6 +354,36 @@ public class CalendarActivity extends AppCompatActivity implements View.OnClickL
 
     @Override
     public void onClick(View v) {
+        //Add event Dialog
+
+        if (v.getId() == R.id.addevent_btnAddEvent){
+
+            //Add the event to DB
+            if (manualAddEvent() == true){
+                //Send back to calendar Page
+                Intent intent = new Intent(
+                        getApplicationContext(), CalendarActivity.class);
+                startActivity(intent);
+            }
+            new GetAllEventsTask().execute();
+        } else if(v.getId() == R.id.addevent_btnCancelEvent){
+
+            //Send back to calendar Page
+            Intent intent = new Intent(
+                    getApplicationContext(), CalendarActivity.class);
+            startActivity(intent);
+        } else if(v.getId() == R.id.btnStartDatePicker){
+            dateFlag = 0;
+            DialogFragment datePicker = new DatePickerFragment();
+            datePicker.show(getSupportFragmentManager(), "date picker");
+
+        } else if(v.getId() == R.id.btnEndDatePicker){
+            dateFlag = 1;
+            DialogFragment datePicker = new DatePickerFragment();
+            datePicker.show(getSupportFragmentManager(), "date picker");
+
+        }
+
         // Side nav
         if (v.getId() == R.id.linkHome) {
             Intent intent = new Intent(
@@ -343,6 +420,123 @@ public class CalendarActivity extends AppCompatActivity implements View.OnClickL
         }
     }
 
+
+    // Extra methods
+
+    private String unpackageEvents(List<Event> events) {
+        StringBuilder eventString = new StringBuilder();
+        for (Event event : events){
+            eventString.append(event.getEventID())
+                    .append("\n")
+                    .append(event.getTitle())
+                    .append(event.getDesc())
+                    .append(event.getLocation())
+                    .append(event.getStartDate())
+                    .append(event.getEndDate())
+                    .append("\n")
+                    .append("\n");
+        }
+        return eventString.toString();
+    }
+
+    private boolean manualAddEvent(){
+        //Getting date & time of creation
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd_HH:mm:ss");
+        Date date = new Date();
+        Date dateStart, dateEnd;
+
+        //Getting editText values
+        String title = etTitle.getText().toString();
+        String desc = etDescription.getText().toString();
+        String startDate = tvStartDate.getText().toString();
+        String endDate = tvEndDate.getText().toString();
+        String location = etLocation.getText().toString();
+
+
+        //Getting string of current date/time
+        String creation = dateFormat.format(date);
+
+        ArrayList<String> strings = new ArrayList();
+        strings.add(title);
+        strings.add(desc);
+        strings.add(startDate);
+        strings.add(endDate);
+        strings.add(location);
+        strings.add(creation);
+
+        for (int i = 0; i < strings.size(); i++) {
+            String string = strings.get(i);
+            if (string.equals("") || string.isEmpty()){
+                strings.set(i,"Not Provided");
+            }
+        }
+
+
+
+        //Fail Checking
+        if (startDate.isEmpty() || endDate.isEmpty()){
+            Toast.makeText(getApplicationContext(),"Event not added: You must have start & end dates",Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+
+        //Needed to check dates.
+        try {
+            dateStart = new SimpleDateFormat("yyyy-MM-dd").parse(startDate);
+            dateEnd = new SimpleDateFormat("yyyy-MM-dd").parse(endDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            Toast.makeText(getApplicationContext(),"An error has occurred",Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if(dateStart.after(dateEnd)){
+            Toast.makeText(getApplicationContext(),"Event not added: Your end date must be after the start date",Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        //Success
+        Event e = new Event(strings.get(0),
+                strings.get(1),
+                strings.get(2),
+                strings.get(3),
+                strings.get(4),
+                strings.get(5));
+
+        List<Event> manualEvents = new ArrayList<>();
+        manualEvents.add(e);
+        new UpdateOrInsertEvent().execute(manualEvents);
+
+
+        Toast.makeText(getApplicationContext(),"Event added",Toast.LENGTH_SHORT).show();
+
+        return true;
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        java.util.Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, year);
+        cal.set(Calendar.MONTH, month);
+        cal.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        String pattern = "yyyy-MM-dd";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+        String currentDate = simpleDateFormat.format(cal.getTime());
+        //String currentDateString = DateFormat.getDateInstance(DateFormat.DEFAULT).format(cal.getTime());
+
+        if (dateFlag == 0) {
+            //Set as startDate
+            tvStartDate.setText(currentDate);
+        } else if(dateFlag == 1){
+            //Set as endDate
+            tvEndDate.setText(currentDate);
+        }
+
+
+    }
+
+
+    // Async tasks
 
 
     //TODO Heavily adapted from Lab solution 3
@@ -398,21 +592,6 @@ public class CalendarActivity extends AppCompatActivity implements View.OnClickL
 
     }
 
-    private String unpackageEvents(List<Event> events) {
-        StringBuilder eventString = new StringBuilder();
-        for (Event event : events){
-            eventString.append(event.getEventID())
-                    .append("\n")
-                    .append(event.getTitle())
-                    .append(event.getDesc())
-                    .append(event.getLocation())
-                    .append(event.getStartDate())
-                    .append(event.getEndDate())
-                    .append("\n")
-                    .append("\n");
-        }
-        return eventString.toString();
-    }
 
     class GetAllEventsTask extends AsyncTask<Void, Void, List<Event>>{
         @Override
