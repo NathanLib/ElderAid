a91aa7fb108a17d343d03661a3085d4f3d1ed33f
Nathan <nathan.lib19@gmail.com>
2019-12-05 13:53:06 +0000

    Import contacts finished

--- ElderAid/app/src/main/java/uk/ac/rgu/elderaid/ContactActivity.java
+++ ElderAid/app/src/main/java/uk/ac/rgu/elderaid/ContactActivity.java
@@ -3,16 +3,21 @@ package uk.ac.rgu.elderaid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
@@ -36,9 +41,14 @@ public class ContactActivity extends AppCompatActivity implements View.OnClickLi
     public final static String EXTRA_CONTACT_ISFAVOURITE = "uk.ac.rgu.elderaid.CONTACT_ISFAVOURITE";
     public final static String EXTRA_CONTACT_POSITION = "uk.ac.rgu.elderaid.CONTACT_POSITION";
     public final static String EXTRA_CONTACT_FAV_FULL = "uk.ac.rgu.elderaid.CONTACT_FAV_FULL";
+    public final static String EXTRA_CONTACT_SAME = "uk.ac.rgu.elderaid.CONTACT_SAME";
+    public final static String EXTRA_CONTACT_IMPORTED = "uk.ac.rgu.elderaid.CONTACT_IMPORTED";
+
+    public static final int REQUEST_READ_CONTACTS = 79;
 
     private ImageButton btnshowSideNav;
     private Button toolbar_addContact;
+    private Button toolbar_importContact;
     private TextView btnMyCard;
 
     private LinearLayout favouriteContact1;
@@ -63,8 +73,10 @@ public class ContactActivity extends AppCompatActivity implements View.OnClickLi
     private ContactAdapter cAdapter;
 
     private ContactDao contactDao;
-    private Contact contact;
 
+    private List<Contact> imports = new ArrayList<>();
+    private int count_same;
+    private int count_imported;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -121,6 +133,8 @@ public class ContactActivity extends AppCompatActivity implements View.OnClickLi
 
         toolbar_addContact = (Button) findViewById(R.id.toolbar_btnAddContact);
         toolbar_addContact.setOnClickListener(this);
+        toolbar_importContact = (Button) findViewById(R.id.toolbar_btnImportContact);
+        toolbar_importContact.setOnClickListener(this);
 
         btnMyCard = (TextView) findViewById(R.id.contact_tvMyCard);
         btnMyCard.setOnClickListener(new View.OnClickListener() {
@@ -135,15 +149,26 @@ public class ContactActivity extends AppCompatActivity implements View.OnClickLi
         //Get the DAO from the database
         this.contactDao = db.cDao();
 
+        Intent launcher = getIntent();
+        if (launcher.hasExtra(ContactActivity.EXTRA_CONTACT_IMPORTED)) {
+            count_imported = launcher.getIntExtra(ContactActivity.EXTRA_CONTACT_IMPORTED, 0);
+
+            if (launcher.hasExtra(ContactActivity.EXTRA_CONTACT_SAME)) {
+                count_same = launcher.getIntExtra(ContactActivity.EXTRA_CONTACT_SAME, 0);
+
+                Toast.makeText(this, count_imported + " contacts imported \n" + count_same + " duplicate contacts not imported" , Toast.LENGTH_LONG).show();
+            }
+            else {
+                Toast.makeText(this, count_imported + " contacts imported" , Toast.LENGTH_LONG).show();
+            }
+        }
 
         // The code below was adapted from a source on the internet from this point forward.
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("My contacts");
         setSupportActionBar(toolbar);
 
-        // Code from https://www.androidhive.info/2016/01/android-working-with-recycler-view/
         recyclerView_contact = (RecyclerView) findViewById(R.id.rvContact);
-
 
         new GetAllContacts().execute();
     }
@@ -152,7 +177,7 @@ public class ContactActivity extends AppCompatActivity implements View.OnClickLi
         addToFavouriteList(contactList);
 
         if (!favouriteContactList.isEmpty()) {
-            for (int i=0; i < favouriteContactList.size(); i++) {
+            for (int i = 0; i < favouriteContactList.size(); i++) {
                 String[] tab = favouriteContactList.get(i).getName().split(" ");
 
                 tv_favList.get(i).setText(tab[0]);
@@ -163,6 +188,7 @@ public class ContactActivity extends AppCompatActivity implements View.OnClickLi
         if (favouriteContactList.size() >= 3) {
             fav_isFull = true;
         }
+
     }
 
     private void addToFavouriteList(List<Contact> contacts) {
@@ -172,6 +198,80 @@ public class ContactActivity extends AppCompatActivity implements View.OnClickLi
                 positionFav.add(contacts.indexOf(c));
             }
         }
+    }
+
+    private void importContact() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
+            getImportContacts();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.READ_CONTACTS}, REQUEST_READ_CONTACTS);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_READ_CONTACTS: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "You can now import your contacts", Toast.LENGTH_LONG).show();
+                } else {
+                    // permission denied, disable the functionality that depends on this permission.
+                    Toast.makeText(this, "You need to grant the permission to import your contacts", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
+    }
+
+    private ArrayList getImportContacts() {
+        // Code from https://www.tutorialspoint.com/how-to-read-all-contacts-in-android
+
+        String id = "";
+        String name = "";
+        String phoneNumber = "";
+
+        Contact contact;
+
+        ContentResolver cr = getContentResolver();
+
+        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
+                null, null, null, null);
+
+        if ((cur != null ? cur.getCount() : 0) > 0) {
+
+            while (cur != null && cur.moveToNext()) {
+                id = cur.getString(
+                        cur.getColumnIndex(ContactsContract.Contacts._ID));
+
+                name = cur.getString(cur.getColumnIndex(
+                        ContactsContract.Contacts.DISPLAY_NAME));
+
+                if (cur.getInt(cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0) {
+                    Cursor pCur = cr.query(
+                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                            null,
+                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                            new String[]{id}, null);
+
+                    while (pCur.moveToNext()) {
+                        phoneNumber = pCur.getString(pCur.getColumnIndex(
+                                ContactsContract.CommonDataKinds.Phone.NUMBER));
+
+                        contact = new Contact(name, phoneNumber, "", false);
+                        imports.add(contact);
+                    }
+
+                    pCur.close();
+                }
+            }
+        }
+        if (cur != null) {
+            cur.close();
+        }
+
+        new InsertImportContact().execute(imports);
+
+        return null;
     }
 
     public void openNavDialog() {
@@ -223,7 +323,6 @@ public class ContactActivity extends AppCompatActivity implements View.OnClickLi
         sideNavDialog.show();
     }
 
-
     public void openMyCardDialog() {
         final Dialog myCardDialog = new Dialog(this);
 
@@ -242,8 +341,6 @@ public class ContactActivity extends AppCompatActivity implements View.OnClickLi
         myCardDialog.show();
 
     }
-
-    //    ***********************************
 
     private void launchSOSPhone() {
         // create the Intent with the action to dial
@@ -336,6 +433,8 @@ public class ContactActivity extends AppCompatActivity implements View.OnClickLi
             intent.putExtra(EXTRA_CONTACT_FAV_FULL, fav_isFull);
 
             startActivity(intent);
+        } else if (v.getId() == R.id.toolbar_btnImportContact) {
+            importContact();
         }
     }
 
@@ -354,12 +453,26 @@ public class ContactActivity extends AppCompatActivity implements View.OnClickLi
         startActivity(intent);
     }
 
-    public void recyclerViewInitialization(){
+    public void recyclerViewInitialization() {
+        // Code from https://www.androidhive.info/2016/01/android-working-with-recycler-view/
         cAdapter = new ContactAdapter(contactList, this);
+
         RecyclerView.LayoutManager cLayoutManager = new LinearLayoutManager(getApplicationContext());
+
         recyclerView_contact.setLayoutManager(cLayoutManager);
         recyclerView_contact.setItemAnimator(new DefaultItemAnimator());
         recyclerView_contact.setAdapter(cAdapter);
+    }
+
+    public void refresh() {
+        Intent intent = new Intent(
+                getApplicationContext(), ContactActivity.class);
+
+        if (count_same > 0) {
+            intent.putExtra(EXTRA_CONTACT_SAME, count_same);
+        }
+        intent.putExtra(EXTRA_CONTACT_IMPORTED, imports.size());
+        startActivity(intent);
     }
 
     //This is an Async task
@@ -375,7 +488,6 @@ public class ContactActivity extends AppCompatActivity implements View.OnClickLi
             super.onPostExecute(contacts);
 
             for (Contact c : contacts) {
-                // Log.d("Contacts", c.toString());
                 contactList.add(c);
             }
 
@@ -384,6 +496,32 @@ public class ContactActivity extends AppCompatActivity implements View.OnClickLi
         }
     }
 
+    class InsertImportContact extends AsyncTask<List<Contact>, Void, Void> {
+        @Override
+        protected Void doInBackground(List<Contact>... importContacts) {
+            List<Contact> imports;
+
+            //Get a list of all the contacts already on the Database
+            List<Contact> allContacts = contactDao.getContacts();
+            count_same = 0;
+            imports = importContacts[0];
+
+            for (Contact c : imports) {
+                for (int i = 0; i < allContacts.size(); i++) {
+                    if (c.getPhoneNum().equals(allContacts.get(i).getPhoneNum())) {
+                        count_same++;
+                        imports.remove(c);
+                    }
+                }
+            }
+
+            contactDao.insertContacts((Contact[]) imports.toArray(new Contact[imports.size()]));
+
+            refresh();
+
+            return null;
+        }
+    }
 }
 
 
